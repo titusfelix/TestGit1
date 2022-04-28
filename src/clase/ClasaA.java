132c1b7acec7ef317b0a2b9c44c8065ec4ddf741
Furtuna Titus <felix.furtuna@ie.ase.ro>
2022-04-28 19:25:42 +0300

    Modificare test

--- src/clase/ClasaA.java
+++ src/clase/ClasaA.java
@@ -2,9 +2,11 @@ package clase;
 
 public class ClasaA {
     private final int a;
+    private final double b;
 
-    public ClasaA(int a) {
+    public ClasaA(int a,double b) {
         this.a = a;
+        this.b=b;
     }
 
     public int getA() {
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,7 @@ import clase.ClasaA;
 
 public class Main {
     public static void main(String[] args) {
-        ClasaA a=new ClasaA(20);
+        ClasaA a=new ClasaA(20,3.14);
         System.out.println(a.getA());
     }
 }
