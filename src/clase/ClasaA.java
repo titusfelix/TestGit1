bfb172e1542db6236fc9bee0ba8aa9b5555352cb
Furtuna Titus <felix.furtuna@ie.ase.ro>
2022-04-28 18:27:00 +0300

    Initial commit

---
+++ src/clase/ClasaA.java
@@ -0,0 +1,13 @@
+package clase;
+
+public class ClasaA {
+    private final int a;
+
+    public ClasaA(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import clase.ClasaA;
+
+public class Main {
+    public static void main(String[] args) {
+        ClasaA a=new ClasaA(20);
+        System.out.println(a.getA());
+    }
+}
